824906d62ac71e8a6dd3ec2f2632be809d1ce08c
Md Bokul Mia <mdbokulmia686@gmail.com>
2025-01-27 14:49:46 +0600

    text file added by me

---
+++ vector.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //vector<int>v={1,2,3,4,5};
+    vector<int>v(5,1);
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i];
+    }
+}
