7327ebe7c6ab91d69be25b91e44f969a65e8cfbb
Md Bokul Mia <mdbokulmia686@gmail.com>
2025-01-27 14:59:26 +0600

    text file added by me

---
+++ Y_Range_sum_query.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long n,q;
+    cin>>n>>q;
+    long long a[n];
+    for (long long i = 0; i < n; i++)
+    {
+        cin>>a[i];
+    }
+    long long pre[n];
+    pre[0]=a[0];
+    for(long long i=1;i<n;i++)
+    {
+        pre[i]=a[i]+pre[i-1];
+    }
+    while(q--)
+    {
+        long long l,r;
+        cin>>l>>r;
+        l--;
+        r--;
+        long long sum=0;
+        if(l==0) sum=pre[r];
+        else  sum= pre[r]-pre[l-1];
+        cout<<sum<<endl;
+        
+    }
+    
+   return  0;
+}
+
+
